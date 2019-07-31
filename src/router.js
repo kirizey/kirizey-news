7bb4698d60a5194df2c75de955e50959609dc5e9
Kirill Valirskiy <kirizey@MacBook-Air-123.routers.saritasa.io>
2019-07-31 19:17:11 +0700

    Auth guard

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import firebase from 'firebase/app';
+
 import LoginPage from './views/LoginPage.vue';
 import PageNotFound from './views/PageNotFound.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -25,9 +27,22 @@ export default new Router({
     {
       path: '/news',
       name: 'news',
-      meta: { layout: 'client' },
+      meta: { layout: 'client', auth: true },
       component: () => import('./views/NewsList.vue')
     },
     { path: '*', component: PageNotFound }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requireAuth = to.matched.some(record => record.meta.auth);
+
+  if (requireAuth && !currentUser) {
+    next('/login');
+  } else {
+    next();
+  }
+});
+
+export default router;
