95257e92a2a96b7b238f279fb14954fba892e98d
Kirill Valirskiy <kirizey@MacBook-Air-123.routers.saritasa.io>
2019-07-31 11:04:14 +0700

    Base structure

--- src/router.js
+++ src/router.js
@@ -1,26 +1,31 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from './views/Login.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    { path: '/', redirect: '/news' },
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: '/login',
+      name: 'login',
+      meta: { layout: 'auth' },
+      component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: '/register',
+      name: 'register',
+      meta: { layout: 'auth' },
+      component: () => import('./views/Register.vue')
+    },
+    {
+      path: '/news',
+      name: 'news',
+      meta: { layout: 'client' },
+      component: () => import('./views/News.vue')
     }
   ]
 });
---
+++ src/views/News.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>news works</p>
+</template>
