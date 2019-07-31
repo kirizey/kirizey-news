9915db0faee55e27bd279e97022ba6e60ddef342
Kirill Valirskiy <kirizey@MacBook-Air-123.routers.saritasa.io>
2019-07-31 11:12:36 +0700

    Connect firebase

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDzGKvBQw16zft04JKSkuKt_8Qrteah2VA',
+  authDomain: 'vue-news-61265.firebaseapp.com',
+  databaseURL: 'https://vue-news-61265.firebaseio.com',
+  projectId: 'vue-news-61265',
+  storageBucket: '',
+  messagingSenderId: '414944129244',
+  appId: '1:414944129244:web:d5af647979400c2e'
+};
--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ import router from './router';
 import store from './store';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+import { firebaseConfig } from '../firebase.config';
 
 Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
